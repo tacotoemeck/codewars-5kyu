a775b9a4f8ca1716356413efa59864f8b4252858
Tomek Wagner <53991292+tacotoemeck@users.noreply.github.com>
2019-10-16 09:15:16 +0100

    Add files via upload

---
+++ firstNonRepeatingChar.js
@@ -0,0 +1,12 @@
+function firstNonRepeatingLetter(s) {
+    
+    for ( let i=0; i < s.length; i++) {
+       let c = s.charAt(i);
+       if ( s.indexOf(c) == i && s.indexOf(c, i + 1) == -1) {
+           return c
+       }
+    }
+    return null
+  }
+
+  firstNonRepeatingLetter('stress')
