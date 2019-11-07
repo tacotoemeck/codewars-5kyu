8a89042093374236c2e3d100193b5e38a381829d
Tomek Wagner <53991292+tacotoemeck@users.noreply.github.com>
2019-11-07 09:44:54 +0000

    Add files via upload

---
+++ sumOfPairs.js
@@ -0,0 +1,43 @@
+// Given a list of integers and a single sum value, return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+
+// sum_pairs([11, 3, 7, 5],         10)
+// #              ^--^      3 + 7 = 10
+// == [3, 7]
+
+// sum_pairs([4, 3, 2, 3, 4],         6)
+// #          ^-----^         4 + 2 = 6, indices: 0, 2 *
+// #             ^-----^      3 + 3 = 6, indices: 1, 3
+// #                ^-----^   2 + 4 = 6, indices: 2, 4
+// #  * entire pair is earlier, and therefore is the correct answer
+// == [4, 2]
+
+// sum_pairs([0, 0, -2, 3], 2)
+// #  there are no pairs of values that can be added to produce 2.
+// == None/nil/undefined (Based on the language)
+
+// sum_pairs([10, 5, 2, 3, 7, 5],         10)
+// #              ^-----------^   5 + 5 = 10, indices: 1, 5
+// #                    ^--^      3 + 7 = 10, indices: 3, 4 *
+// #  * entire pair is earlier, and therefore is the correct answer
+// == [3, 7]
+
+
+function sum_pairs(arr, num) {
+    
+    let previous;
+
+    for ( let i = 0; i < arr.length; i++) {
+       if ( previous && previous == arr[i]) continue;
+       previous = arr[i];
+
+       let section = arr.slice(0, i);
+       let sum = section.indexOf(num - arr[i])
+
+       if ( sum !== -1 ) {
+           return [section[sum], arr[i]]
+       }
+    }
+}
+
+sum_pairs([4, 3, 2, 3, 4],6)
+
