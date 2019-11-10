2878dfaaf648cff824c264fd7715507d69b76d1d
Tomek Wagner <53991292+tacotoemeck@users.noreply.github.com>
2019-11-10 13:02:43 +0000

    Add files via upload

---
+++ notVerySecure.js
@@ -0,0 +1,14 @@
+// In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
+
+// The string has the following conditions to be alphanumeric:
+
+// At least one character ("" is not valid)
+// Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+// No whitespaces / underscore
+
+function alphanumeric(string){
+    if ( string == "" ) return false;
+      
+      let regex = RegExp(/[^a-zA-Z0-9]/i);
+      return !regex.test(string)
+    }
